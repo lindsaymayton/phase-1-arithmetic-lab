e2848b09072f893a365be21dcd9ece75f412f93b
LindsayMayton <lindsaymayton@yahoo.com>
2023-10-15 20:45:06 -0700

    mathIsAWonderfulThing

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply); // Output: 62
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+let num3 = 15;
+let num4 = 11;
+
+let mod = num3 % num4;
+console.log(mod);
+let numbers = [10, 5, 20, 15];
+let max = Math.max(...numbers);
+console.log(max); // Output: 20
